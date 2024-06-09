e52b5834b2d3ce43cf3a5af0975ff48f1985e34b
Antonio Lucero <alucero@darient.com>
2024-06-09 18:10:27 -0400

    layout adjustments

--- src/app/modules/auth/pages/login/login.component.ts
+++ src/app/modules/auth/pages/login/login.component.ts
@@ -2,13 +2,12 @@ import { Component } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
 import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatGridListModule } from "@angular/material/grid-list";
 import { MatInputModule } from "@angular/material/input";
 
 @Component({
     selector: "app-login",
     standalone: true,
-    imports: [MatCardModule, MatInputModule, MatFormFieldModule, MatGridListModule, MatButtonModule],
+    imports: [MatCardModule, MatInputModule, MatFormFieldModule, MatButtonModule],
     templateUrl: "./login.component.html",
     styleUrl: "./login.component.scss"
 })
