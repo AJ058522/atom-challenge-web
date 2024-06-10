c0b674f9f96719cb4fa48d8aad98928077bf140b
Antonio Lucero <alucero@darient.com>
2024-06-10 17:12:20 -0400

    deleted not used component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,11 +9,6 @@ export const routes: Routes = [
         pathMatch: "full"
     },
     {
-        path: "home",
-        loadComponent: () => import("./modules/example-page/example-page.component").then((m) => m.ExamplePageComponent)
-
-    },
-    {
         path: "login",
         loadComponent: () => import("./modules/auth/pages/login/login.component").then((m) => m.LoginComponent)
     },
